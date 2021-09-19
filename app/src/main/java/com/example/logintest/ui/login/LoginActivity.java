9f13ccbc8a57f755f71a19792761e5d94bd6e387
jwd5062 <jwd5062@g.rit.edu>
2021-09-19 17:22:03 -0400

    Added stuff to the settings page
    Also need to track down a problem with the main menu

--- app/src/main/java/com/example/logintest/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/logintest/ui/login/LoginActivity.java
@@ -1,27 +1,20 @@
 package com.example.logintest.ui.login;
 
 import android.app.Activity;
-
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.annotation.Nullable;
-import androidx.annotation.StringRes;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.StringRes;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.logintest.R;
 
@@ -41,41 +34,35 @@ public class LoginActivity extends AppCompatActivity {
         final Button loginButton = findViewById(R.id.button_login);
         final ProgressBar loadingProgressBar = findViewById(R.id.loading);
 
-        loginViewModel.getLoginFormState().observe(this, new Observer<LoginFormState>() {
-            @Override
-            public void onChanged(@Nullable LoginFormState loginFormState) {
-                if (loginFormState == null) {
-                    return;
-                }
-                loginButton.setEnabled(loginFormState.isDataValid());
-                if (loginFormState.getUsernameError() != null) {
-                    usernameEditText.setError(getString(loginFormState.getUsernameError()));
-                }
-                if (loginFormState.getPasswordError() != null) {
-                    passwordEditText.setError(getString(loginFormState.getPasswordError()));
-                }
+        loginViewModel.getLoginFormState().observe(this, loginFormState -> {
+            if (loginFormState == null) {
+                return;
+            }
+            loginButton.setEnabled(loginFormState.isDataValid());
+            if (loginFormState.getUsernameError() != null) {
+                usernameEditText.setError(getString(loginFormState.getUsernameError()));
+            }
+            if (loginFormState.getPasswordError() != null) {
+                passwordEditText.setError(getString(loginFormState.getPasswordError()));
             }
         });
 
-        loginViewModel.getLoginResult().observe(this, new Observer<LoginResult>() {
-            @Override
-            public void onChanged(@Nullable LoginResult loginResult) {
-                if (loginResult == null) {
-                    return;
-                }
-                loadingProgressBar.setVisibility(View.GONE);
-                if (loginResult.getError() != null) {
-                    showLoginFailed(loginResult.getError());
-                }
-                if (loginResult.getSuccess() != null) {
-                    updateUiWithUser(loginResult.getSuccess());
-                }
-                setResult(Activity.RESULT_OK);
-
-                //Complete and destroy login activity once successful
-                //finish will minimize and "end"
-                //finish();
+        loginViewModel.getLoginResult().observe(this, loginResult -> {
+            if (loginResult == null) {
+                return;
             }
+            loadingProgressBar.setVisibility(View.GONE);
+            if (loginResult.getError() != null) {
+                showLoginFailed(loginResult.getError());
+            }
+            if (loginResult.getSuccess() != null) {
+                updateUiWithUser(loginResult.getSuccess());
+            }
+            setResult(Activity.RESULT_OK);
+
+            //Complete and destroy login activity once successful
+            //finish will minimize and "end"
+            //finish();
         });
 
         TextWatcher afterTextChangedListener = new TextWatcher() {
@@ -97,26 +84,19 @@ public class LoginActivity extends AppCompatActivity {
         };
         usernameEditText.addTextChangedListener(afterTextChangedListener);
         passwordEditText.addTextChangedListener(afterTextChangedListener);
-        passwordEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_DONE) {
-                    loginViewModel.login(usernameEditText.getText().toString(),
-                            passwordEditText.getText().toString());
-                }
-                return false;
-            }
-        });
-
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                loadingProgressBar.setVisibility(View.VISIBLE);
+        passwordEditText.setOnEditorActionListener((v, actionId, event) -> {
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
                 loginViewModel.login(usernameEditText.getText().toString(),
                         passwordEditText.getText().toString());
-                openActivityDashboard();
             }
+            return false;
+        });
+
+        loginButton.setOnClickListener(v -> {
+            loadingProgressBar.setVisibility(View.VISIBLE);
+            loginViewModel.login(usernameEditText.getText().toString(),
+                    passwordEditText.getText().toString());
+            openActivityDashboard();
         });
     }
 
--- app/src/main/java/com/example/logintest/ui/login/Settings.java
+++ app/src/main/java/com/example/logintest/ui/login/Settings.java
@@ -1,8 +1,11 @@
 package com.example.logintest.ui.login;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.logintest.R;
 
@@ -12,5 +15,50 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        Button aboutButton = (Button) findViewById(R.id.button_about);
+        Button styleButton = (Button) findViewById(R.id.button_style);
+        Button clearDataButton = (Button) findViewById(R.id.button_clear_data);
+        Button returnMainButton = (Button) findViewById(R.id.button_return_main);
+
+        aboutButton.setOnClickListener(v -> openAboutActivity());
+        styleButton.setOnClickListener(v -> openStyleActivity());
+        clearDataButton.setOnClickListener(v -> confirmClear());
+        returnMainButton.setOnClickListener(v -> openDashboardActivity());
+    }
+
+    private void openAboutActivity() {
+        Intent intent = new Intent(this, SettingsAbout.class);
+        startActivity(intent);
+    }
+
+    private void openStyleActivity() {
+        Intent intent = new Intent(this, SettingsStyle.class);
+        startActivity(intent);
+    }
+
+    private void confirmClear() {
+
+        // TODO: Clear patient data...reset app to default
+        new AlertDialog.Builder(this)
+//                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle("Clear Patient Info")
+                .setMessage("Are you sure you want to clear all patient info?")
+                .setPositiveButton("Yes", (dialog, which) -> finish())
+                .setNegativeButton("No", null)
+                .show();
+
+        Intent intent = new Intent(this, Dashboard.class);
+        startActivity(intent);
+        finish();
+    }
+
+
+
+    private void openDashboardActivity() {
+        Intent intent = new Intent(this, Dashboard.class);
+        startActivity(intent);
+        // make sure to close this activity, since we aren't returning to it
+        this.finish();
     }
 }
