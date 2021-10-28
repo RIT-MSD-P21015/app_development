8bd029f70286a7f5415386357f41e04248835ce3
Paul <pjk2563@rit.edu>
2021-10-27 21:20:42 -0400

    network based login implemented

--- app/src/main/java/com/example/logintest/data/LoginDataSource.java
+++ app/src/main/java/com/example/logintest/data/LoginDataSource.java
@@ -1,27 +1,36 @@
 package com.example.logintest.data;
 
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
 import com.example.logintest.data.model.LoggedInUser;
 
+import org.json.JSONObject;
+
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 /**
  * Class that handles authentication w/ login credentials and retrieves user information.
  */
 public class LoginDataSource {
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     public Result<LoggedInUser> login(String username, String password) {
 
         try {
+            // POST email and password to /api/token
+            String token = NetworkManager.getToken(username, password);
 
-            // TODO POST email and password to /api/token
-
-            // TODO check if token received
-            String token = null;
-
-            if (token == null) {
-                // TODO get first and last name from /api/user
-                String firstName = "John";
-                String lastName = "Doe";
+            // check if token received
+            if (!token.isEmpty()) {
+                // get first and last name from /api/user
+                String json = NetworkManager.sendGET("/api/user", token);
+                JSONObject jsonObject = new JSONObject(json);
+                String firstName = jsonObject.getString("firstname");
+                String lastName = jsonObject.getString("lastname");;
 
                 String displayName = firstName + " " + lastName;
                 LoggedInUser currentUser = new LoggedInUser(token, displayName);
--- app/src/main/java/com/example/logintest/data/NetworkManager.java
+++ app/src/main/java/com/example/logintest/data/NetworkManager.java
@@ -1,6 +1,13 @@
 package com.example.logintest.data;
 
+import android.os.Build;
 import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
@@ -10,23 +17,37 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 public class NetworkManager {
 
     private final static String BaseURL = "http://fallriskdb-vm.main.ad.rit.edu:5000";
 
-    public static int sendGET() throws IOException {
-        URL url = new URL(BaseURL);
+    // returns page data
+    public static String sendGET(String location, String token) throws IOException {
+        URL url = new URL(BaseURL.concat(location));
         HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        urlConnection.setRequestMethod("GET");
+        if(!token.isEmpty()) {
+            String authHeaderValue = "Bearer " + token;
+            urlConnection.setRequestProperty("Authorization", authHeaderValue);
+        }
+
+        String json = "";
         try {
             InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-            String test = readStream(in);
-            Log.d("STATE",test);
-
+            json = readStream(in);
+            Log.d("STATE",json);
         } finally {
             urlConnection.disconnect();
         }
-        return urlConnection.getResponseCode();
+        if(urlConnection.getResponseCode() == 200) {
+            return json;
+        }
+        else {
+            return "Bad response";
+        }
     }
     private static String readStream(InputStream is) throws IOException {
         StringBuilder sb = new StringBuilder();
@@ -61,5 +82,43 @@ public class NetworkManager {
         }
         // TODO better error handling?
         return con.getResponseCode();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public static String getToken(String username, String password) throws IOException {
+
+        String auth = username + ":" + password;
+        byte[] authBytes = auth.getBytes(StandardCharsets.UTF_8);
+        String encodedAuth = "bad";
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            encodedAuth = Base64.getEncoder().encodeToString(authBytes);
+        }
+        String authHeaderValue = "Basic " + encodedAuth;
+
+        URL url = new URL(BaseURL.concat("/api/tokens"));
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("POST");
+        con.setRequestProperty("Content-Type", "application/json; utf-8");
+        con.setRequestProperty("Accept", "application/json");
+        con.setRequestProperty("Authorization", authHeaderValue);
+        con.setDoOutput(true);
+
+        String token = "";
+        try(BufferedReader br = new BufferedReader(
+                new InputStreamReader(con.getInputStream(), "utf-8"))) {
+            StringBuilder response = new StringBuilder();
+            String responseLine = null;
+            while ((responseLine = br.readLine()) != null) {
+                response.append(responseLine.trim());
+            }
+            Log.d("sendPost", response.toString());
+            JSONObject json = new JSONObject(response.toString());
+
+            token = json.getString("token");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return token;
     }
 }
