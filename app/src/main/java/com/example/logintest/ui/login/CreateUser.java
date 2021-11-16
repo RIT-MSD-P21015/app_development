44819a85469744b7955bdad7ad14fda3ecec0e06
jwd5062 <jwd5062@g.rit.edu>
2021-11-15 19:37:48 -0500

    Added CreateUser error checking
    Added email validation, checking for missing entries, password confirmation match

--- app/src/main/java/com/example/logintest/ui/login/CreateUser.java
+++ app/src/main/java/com/example/logintest/ui/login/CreateUser.java
@@ -1,8 +1,12 @@
 package com.example.logintest.ui.login;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.text.TextUtils;
+import android.util.Log;
+import android.util.Patterns;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -16,6 +20,8 @@ import java.io.IOException;
 
 public class CreateUser extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,8 +32,8 @@ public class CreateUser extends AppCompatActivity {
         StrictMode.setThreadPolicy(policy);
 
         // Grab all the stuff on screen
-        Button createAccount = findViewById(R.id.buttonCreateAccount);
-        Button returnDashboardButton = findViewById(R.id.buttonReturnMainCreateAccount);
+        Button createAccountButton = findViewById(R.id.buttonCreateAccount);
+        Button returnLoginButton = findViewById(R.id.buttonReturnMainCreateAccount);
         TextView textViewCreateAccount = findViewById(R.id.textViewCreateAccount);
         EditText editTextFirstName = findViewById(R.id.editTextCreateFirstName);
         EditText editTextLastName = findViewById(R.id.editTextCreateLastName);
@@ -46,45 +52,84 @@ public class CreateUser extends AppCompatActivity {
         editTextCreateConfirmPassword.setTextSize(SettingsStyle.getFontSize());
 
         // Set size of buttons
-//        createAccount.setTextSize(SettingsStyle.getFontSize());
-//        returnDashboardButton.setTextSize(SettingsStyle.getFontSize());
+//        createAccountButton.setTextSize(SettingsStyle.getFontSize());
+//        returnLoginButton.setTextSize(SettingsStyle.getFontSize());
 
         // Set the on click listener
         // TODO leave disabled until all data good
-        createAccount.setOnClickListener(v -> {
-            createAccount(
-                    editTextFirstName.getText().toString(),
-                    editTextLastName.getText().toString(),
-                    editTextCreateEmail.getText().toString(),
-                    editTextCreatePassword.getText().toString()
-            );
-        });
-        returnDashboardButton.setOnClickListener(v -> openDashboardActivity());
+//        createAccountButton.setEnabled(true);
+        createAccountButton.setOnClickListener(v -> createAccount(
+                editTextFirstName.getText().toString(),
+                editTextLastName.getText().toString(),
+                editTextCreateEmail.getText().toString(),
+                editTextCreatePassword.getText().toString(),
+                editTextCreateConfirmPassword.getText().toString()
+        ));
+        returnLoginButton.setOnClickListener(v -> openLoginActivity());
     }
 
-    private void createAccount(String firstName, String lastName, String email, String passwd) {
-        String body = "{ \"firstname\" : \"" + firstName + "\", \"lastname\" : \"" + lastName + "\", " +
-                "\"email\" : \"" + email + "\", \"password\" : \"" + passwd + "\" }";
+    private void createAccount(String firstName, String lastName, String email, String password, String confirmPassword) {
+        AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+        Log.d("FIRSTNAME:",firstName);
+        Log.d("LASTNAME:",lastName);
+        if (!password.equals(confirmPassword) || password.isEmpty() || password.length()<=5){
+            alertDialog.setTitle("Error");
+            alertDialog.setMessage("Confirm password does not match password, or empty password");
+            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                    (dialog, which) -> dialog.dismiss());
+            alertDialog.show();
+        }
+        else if (firstName.isEmpty() || lastName.isEmpty()){
+            alertDialog.setTitle("Error");
+            alertDialog.setMessage("First name or last name is not provided");
+            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                    (dialog, which) -> dialog.dismiss());
+            alertDialog.show();
+        }
+        else if (!(!TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches())){
+            alertDialog.setTitle("Error");
+            alertDialog.setMessage("Email is not provided or not valid");
+            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                    (dialog, which) -> dialog.dismiss());
+            alertDialog.show();
+        }
+        else {
+            // TODO Add something to catch if no server response
 
-        // check response for existing email or success
-        int code = 0;
-        try {
-            code = NetworkManager.sendPost("/api/user",body);
-        } catch (IOException e) {
-            e.printStackTrace();
+            String body = "{ \"firstname\" : \"" + firstName + "\", \"lastname\" : \"" + lastName + "\", " +
+                    "\"email\" : \"" + email + "\", \"password\" : \"" + password + "\" }";
+
+            // check response for existing email or success
+            int serverResponseCode = 0;
+            try {
+                serverResponseCode = NetworkManager.sendPost("/api/user", body);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            // on success, move to openDashboardActivity
+            if (serverResponseCode == 201) {
+                alertDialog.setTitle("");
+                alertDialog.setMessage("Account Created Successfully");
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                        (dialog, which) -> dialog.dismiss());
+                alertDialog.show();
+                openLoginActivity();
+            } else {
+                alertDialog.setTitle("Alert");
+                alertDialog.setMessage("Account with that email already exists");
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                        (dialog, which) -> dialog.dismiss());
+                alertDialog.show();
+            }
         }
 
-        // on success, move to openDashboardActivity
-        if(code == 201) {
-            // TODO Create a popup that says account created
-            openDashboardActivity();
-        }
-
-        // TODO on failure, pop up warning existing user
+        // TODO test popups when server is up and running
     }
 
-    private void openDashboardActivity() {
+    private void openLoginActivity() {
         // TODO how to set up login with new info?
+        // I don't know what this means @Paul
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
         // make sure to close this activity, since we aren't returning to it
