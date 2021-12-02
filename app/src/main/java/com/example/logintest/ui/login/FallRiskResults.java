bfbb5a04633ef6352d24466b22be2928465e350a
jwd5062 <jwd5062@g.rit.edu>
2021-12-01 23:41:28 -0500

    Fixed fallriskresult not updating the background color

--- app/src/main/java/com/example/logintest/ui/login/FallRiskResults.java
+++ app/src/main/java/com/example/logintest/ui/login/FallRiskResults.java
@@ -31,7 +31,8 @@ public class FallRiskResults extends AppCompatActivity {
         String resultString = (int) (Results.getResultDouble()*100) + "%";
         ResultTextView.setText(resultString);
 
-
+        // Set color of button backgrounds
+        returnToDashboard.setBackgroundColor(SettingsStyle.getPrimaryColor());
 
         TimeZone tz = TimeZone.getDefault();
         TimeZone UTC = TimeZone.getTimeZone("UTC");
